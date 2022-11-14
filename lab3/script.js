0ee2b79e96dcb3a623d7b6ae3560a66fa9bf9161
usama790 <80891551+usama790@users.noreply.github.com>
2022-11-14 12:17:30 +0000

    Update script.js

--- lab3/script.js
+++ lab3/script.js
@@ -1,1 +1,96 @@
 
+const addBtn = document.getElementById('add')  
+ const notes = JSON.parse(localStorage.getItem('notes'))  
+ if(notes) {  
+   notes.forEach(note => addNewNote(note))  
+ }  
+ addBtn.addEventListener('click', () => addNewNote())  
+ function addNewNote(text = '') {  
+   const note = document.createElement('div')  
+   note.classList.add('note')  
+   note.innerHTML = 
+   `  
+   <div class="tools">  
+     <button class="edit"><i class="fas fa-edit"></i></button>  
+     <button class="delete"><i class="fas fa-trash-alt"></i></button>  
+     <button class="red">RED</button>  
+     <button class="blue">BLUE</button> 
+     <button class="green">GREEN</button> 
+     
+     
+     
+   </div>  
+   <div class="main ${text ? "" : "hidden"}"></div>  
+   <textarea id="textarea" class="${text ? "hidden" : ""}"></textarea>  
+   ` 
+   const greenbtn =  note.querySelector('.green') 
+   const redbtn =  note.querySelector('.red') 
+   const bluebtn =  note.querySelector('.blue') 
+   const editBtn = note.querySelector('.edit')  
+   const deleteBtn = note.querySelector('.delete')  
+   const main = note.querySelector('.main')  
+   const textArea = note.querySelector('textarea')  
+   textArea.value = text  
+   main.innerHTML = marked(text) 
+   
+
+
+
+   greenbtn.addEventListener('click', () => {  
+    document.getElementById("textarea").style.backgroundColor = "green";
+ })  
+
+   bluebtn.addEventListener('click', () => {  
+    document.getElementById("textarea").style.backgroundColor = "blue";
+ })  
+
+   redbtn.addEventListener('click', () => {  
+    document.getElementById("textarea").style.backgroundColor = "red";
+
+   
+  })  
+   rxjs.fromEvent(deleteBtn,'click')  
+    .subscribe(() => del()
+
+    
+   )  
+   
+   function del(){
+    note.remove()  
+     
+
+   }
+
+
+   rxjs.fromEvent(editBtn,'click' () 
+   .subscribe(() => edit()
+
+
+     main.classList.toggle('hidden')  
+     textArea.classList.toggle('hidden')  
+   ) 
+
+   function edit(){
+
+    main.classList.toggle('hidden')  
+    textArea.classList.toggle('hidden')  
+     
+     
+
+   }
+
+
+   
+   textArea.addEventListener('input', (e) => {  
+     const { value } = e.target  
+     main.innerHTML = marked(value)  
+     updateLS()  
+   })  
+   document.body.appendChild(note)  
+ }  
+ function updateLS() {  
+   const notesText = document.querySelectorAll('textarea')  
+   const notes = []  
+   notesText.forEach(note => notes.push(note.value))  
+   localStorage.setItem('notes', JSON.stringify(notes))  
+ }  
